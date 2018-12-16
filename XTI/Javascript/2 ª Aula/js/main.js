bfe3f0409d689309fd872d655d2fd591facb18e6
Gabriel Aquino <gabrielaquino.spt3@gmail.com>
2018-12-15 21:19:43 -0300

    2ª Aula - Eventos
    Correção: ajuste de escrita.

--- XTI/Javascript/2 ª Aula/js/main.js
+++ XTI/Javascript/2 ª Aula/js/main.js
@@ -9,7 +9,7 @@ div.innerHTML = `<img id="img" onmouseover="ligaDesliga(this.id)" onmouseout="li
 function ligaDesliga(id) {    
     // define a tag img para a variavel img
     var img = document.getElementById('img');
-    // falida o atribuito src da img na hora do click
+    // valida o atribuito src da img na hora do click
     if(img.src == lampadaApagada){
         // Se for verdade ela acende
         img.src = `${lampadaAcessa}`;
