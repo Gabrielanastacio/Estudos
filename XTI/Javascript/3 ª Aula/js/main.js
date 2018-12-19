3327700f9e4054191435ac1ab37dfb30246c9373
Gabriel Aquino <gabrielaquino.spt3@gmail.com>
2018-12-19 19:25:04 -0300

    3ª Aula - Operadores
    Preparando o ambiente para a terceira aula de operadores.

--- XTI/Javascript/3 ª Aula/js/main.js
+++ XTI/Javascript/3 ª Aula/js/main.js
@@ -1,1 +1,27 @@
+var primeiroValor = document.getElementById('primeiroValor')   
 
+var segundoValor = document.getElementById('segundoValor')
+
+
+
+function calcular(primeiroValor,segundoValor) {
+    
+    if(primeiroValor.value === ''){
+       
+        focusPrimeiroValor(primeiroValor);
+    
+    }else{
+
+        alert('oooi');
+
+    }
+
+}
+
+function focusPrimeiroValor(primeiroValor) {
+    primeiroValor.focus();
+}
+
+function createSpanPrimeiroValor(){
+    
+}
